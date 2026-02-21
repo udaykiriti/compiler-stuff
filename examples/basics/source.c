8d46d9db5a003c98f222824dd1f30fcfa81e92bf
udaykiriti <udaykiriti624@gmail.com>
2026-02-21 17:43:12 +0530

    small stuff

--- examples/basics/source.c
+++ examples/basics/source.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 
-// A simple function to demonstrate arithmetic and optimization potential
 int calculate(int a, int b) {
-    int x = a + 5;
-    int y = b * 2;
-    // Dead code example (opt should remove this if unused)
-    int z = x * 0; 
+   int x = a + 5;
+   int y = b * 2;
+   // Dead code example (opt should remove this if unused)
+   // even if we dont remove it the llvm will take care of it 
+   // it optimizes it...
+   int z = x * 0; 
     
-    if (x > y) {
-        return x - y;
-    } else {
-        return y - x;
-    }
+   if (x > y) {
+     return x - y;
+   } else {
+     return y - x;
+   }
+
+   // return (x > y) ? (x - 5) : (y - x);
 }
 
-int main() {
-    int result = calculate(10, 20);
-    printf("Result: %d\n", result);
+/*unsigned*/int main() {
+    int res = calculate(10, 20);
+    printf("Result: %d\n", res);
     return 0;
 }
 
